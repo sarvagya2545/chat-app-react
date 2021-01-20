3cac77cc9234742cc7259fb8c0bdf30dd0a19590
Sarvagya Sharma <sarvagya2545@gmail.com>
2021-01-20 22:22:40 +0530

    Extract DropDown component to utils

--- client/src/components/chat/ChatHeading.js
+++ client/src/components/chat/ChatHeading.js
@@ -2,23 +2,24 @@ import React, { Component } from 'react';
 // import Avatar from '../../images/avatar.jpg'
 import pfp from '../../images/pfp.svg';
 import ChatPerson from '../chat/ChatPerson';
-import threedots from '../../images/3dots.svg';
-import cx from 'classnames';
 import { connect } from 'react-redux';
 import { openInfoPanel } from '../../redux/actions/uiActions';
 import { exitChatRoom } from '../../redux/actions/chatActions';
+import DropDown from '../utils/DropDown';
 
 class ChatHeading extends Component {
-    state = { isDropDown: false }
-
-    roomInfoClickHandler = e => {
-        this.props.openInfoPanel();
-        this.setState({ isDropDown: false })
-    }
-
-    exitClickHandler = e => {
-        this.props.exitChatRoom(this.props.currentRoomId, this.props.currentRoomName)
-    }
+    listOfItems = [
+        {
+            text: 'Room Info',
+            callback: () => this.props.openInfoPanel(),
+            close: true
+        },
+        {
+            text: 'Exit room',
+            callback: () => this.props.exitChatRoom(this.props.currentRoomId, this.props.currentRoomName),
+            close: false
+        }
+    ];
 
     render() { 
         return (
@@ -27,18 +28,9 @@ class ChatHeading extends Component {
                     <img src={pfp} alt="pfp" className="pfp-img"/>
                 </div>
                 <ChatPerson/>
-                <div 
-                    className={cx('three-dots', 'custom-dropdown', { 'hidden': !this.state.isDropDown })}
-                >
-                    <img 
-                        src={threedots} alt="menu"
-                        onClick={e => this.setState({ isDropDown: !this.state.isDropDown })}
-                    />
-                    <div className="list">
-                        <button className="btn" onClick={this.roomInfoClickHandler}>Room Info</button>
-                        <button className="btn" onClick={this.exitClickHandler}>Exit</button>
-                    </div>
-                </div>
+                <DropDown
+                    listOfItems={this.listOfItems}
+                />
             </div>
         );
     }
---
+++ client/src/components/utils/DropDown.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import cx from "classnames";
+import threedots from "../../images/3dots.svg";
+
+class DropDown extends Component {
+    state = { isDropDown: false };
+
+    handleClick = item => {
+        item.callback();
+        if(item.close) {
+            this.setState({ isDropDown: false })
+        }
+    }
+
+    render() {
+        return (
+            <div
+                className={cx("three-dots", "custom-dropdown", {
+                    hidden: !this.state.isDropDown,
+                })}
+            >
+                <img
+                    src={threedots}
+                    alt="menu"
+                    onClick={(e) => this.setState({ isDropDown: !this.state.isDropDown })}
+                />
+                <div className="list">
+                    {this.props.listOfItems.map(item => {
+                        return (<button className="btn" onClick={e => this.handleClick(item)}>{item.text}</button>)
+                    })}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default DropDown;
