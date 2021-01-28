9151e3302c326b886a56f5cc4df4d146d59bce89
Sarvagya Sharma <sarvagya2545@gmail.com>
2021-01-28 19:12:04 +0530

    UI for change password added

--- client/src/components/chat/UserInfo.js
+++ client/src/components/chat/UserInfo.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import cx from 'classnames';
 import goBack from '../../images/go_back.svg';
+import Pfp from './Pfp';
+import pfp from '../../images/pfp.svg';
+import { connect } from 'react-redux';
 
 class UserInfo extends Component {
   render() { 
+    const { username, email } = this.props;
     return (
       <div className={cx("user-info", { "hidden": !this.props.visible })}>
         <div className="user-info-header">
@@ -12,9 +16,25 @@ class UserInfo extends Component {
             </button>
             <div className="header">User Info</div>
         </div>
+        <div className="user-main-details">
+            <Pfp pfp={pfp} size="xl" input/>
+            <h3>{username}</h3>
+            <p>{email}</p>
+            <div className="actions">
+              <h2>User Actions</h2>
+              <button className="btn btn-submit">Change Password</button>
+            </div>
+        </div>
       </div>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    username: state.auth.user.auth.username,
+    email: state.auth.user.auth.email
+  }
+}
  
-export default UserInfo;
+export default connect(mapStateToProps)(UserInfo);
