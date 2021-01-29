dd432630ec295afef719cc7219acf733c27a399d
Sarvagya Sharma <sarvagya2545@gmail.com>
2021-01-30 01:10:45 +0530

    fix: remove oldpassword field

--- client/src/components/home/PasswordChange.js
+++ client/src/components/home/PasswordChange.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { Component } from 'react';
 import Loader from 'react-loader-spinner';
 import { withRouter } from 'react-router-dom';
+import { tokenConfig } from '../../redux/actions/authActions';
 
 class PasswordChange extends Component {
   state = {
@@ -10,7 +11,6 @@ class PasswordChange extends Component {
     token: null,
     msg: null,
     email: '',
-    oldPassword: '',
     newPassword: '',
     confirmPassword: ''
   }
@@ -32,16 +32,17 @@ class PasswordChange extends Component {
         console.log(res);
         if(res.status === 200) {
           this.setState({ valid: true, token: res.data.newToken, email: res.data.email });
+          this.setState({ loading: false });
         }
       })
       .catch(err => {
         console.log(err.response);
         if(err.response?.status === 401) {
           this.setState({ msg: err.response.data.msg })
+          this.setState({ loading: false });
         }
       });
 
-    this.setState({ loading: false });
 
     // set a timeout for the page to lose it's token after 10 mins
     setTimeout(() => {
@@ -56,7 +57,14 @@ class PasswordChange extends Component {
   onSubmit = e => {
     e.preventDefault();
 
+    const config = tokenConfig(this.state.token);
+
+    const { newPassword, confirmPassword } = this.state;
+
     // SEND AXIOS REQUEST TO SERVER TO CHANGE PASSWORD. ALONG WITH THE TOKEN.
+    axios.post(`/api/users/chng_pwd`, { newPassword, confirmPassword }, config)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
   }
 
   render() { 
@@ -84,10 +92,6 @@ class PasswordChange extends Component {
         >
           <h1 style={{ paddingBottom: '1rem' }}>RESET PASSWORD</h1>
           <p style={{ padding: '0 0 1rem' }}>{this.state.email}</p>
-          <div className="form-group">
-            <input type="password" name="oldPassword" className="input" placeholder="Enter old Password" value={this.state.oldPassword} onChange={this.onChangeHandler}/> 
-            <label htmlFor="oldPassword">Enter old Password</label>
-          </div>
           <div className="form-group">
             <input type="password" name="newPassword" className="input" placeholder="Enter new Password" value={this.state.newPassword} onChange={this.onChangeHandler}/> 
             <label htmlFor="password">Enter new Password</label>
