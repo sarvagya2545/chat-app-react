e8a7444597ea1a2d266d3db6c43433acea268fb6
Sarvagya Sharma <sarvagya2545@gmail.com>
2021-02-10 12:03:52 +0530

    firebase debug

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -10,3 +10,5 @@ export const app = firebase.initializeApp({
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 });
+
+console.log(app);
