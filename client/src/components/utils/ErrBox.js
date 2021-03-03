a41fe6c90853c72b93d20339ee7e7fdbc2e6b08f
Sarvagya Sharma <sarvagya2545@gmail.com>
2021-03-03 19:23:01 +0530

    error box responsive

--- client/src/components/utils/ErrBox.js
+++ client/src/components/utils/ErrBox.js
@@ -12,20 +12,23 @@ class ErrBox extends Component {
     render() { 
         const { isError, errType, statusCode, errArray, clearErrors } = this.props;
         return (
-            <div className={cx("err-box", { "visible": isError })}>
-                <div className="head">
-                    <h2>Error!</h2>
-                    <button className="btn btn-close close" onClick={e => clearErrors()}>&#x2716;</button>
+            <>
+                <div className={cx("err-box-filter", { "visible": isError })}/>
+                <div className={cx("err-box", { "visible": isError })}>
+                    <div className="head">
+                        <h2>Error!</h2>
+                        <button className="btn btn-close close" onClick={e => clearErrors()}>&#x2716;</button>
+                    </div>
+                    <div className="middle">
+                        <p className="err-status">{statusCode}</p>
+                        <p className="err-type">{errType}</p>
+                    </div>
+                    <div className="errors">
+                        {errArray.map((err, index) => <p className="err-message" key={index}>{err}</p>)}
+                    </div>
+                    <button className="btn btn-submit" onClick={e => clearErrors()}>OK</button>
                 </div>
-                <div className="middle">
-                    <p className="err-status">{statusCode}</p>
-                    <p className="err-type">{errType}</p>
-                </div>
-                <div className="errors">
-                    {errArray.map((err, index) => <p className="err-message" key={index}>{err}</p>)}
-                </div>
-                <button className="btn btn-submit" onClick={e => clearErrors()}>OK</button>
-            </div>
+            </>
         );
     }
 }
